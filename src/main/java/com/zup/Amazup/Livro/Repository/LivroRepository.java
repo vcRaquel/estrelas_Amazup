ebb4f78ad8b7cc7d4483465d135a37104dec1511
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:08:31 -0300

    criar classe LivroRepository

--- src/main/java/com/zup/Amazup/Livro/Repository/LivroRepository.java
+++ src/main/java/com/zup/Amazup/Livro/Repository/LivroRepository.java
@@ -1,2 +1,7 @@
-package com.zup.Amazup.Livro.Repository;public interface LivroRepository {
+package com.zup.Amazup.Livro.Repository;
+
+import com.zup.Amazup.Livro.Livro;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LivroRepository extends CrudRepository<Livro, Integer> {
 }
