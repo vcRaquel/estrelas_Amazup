d2ae0e3b3e70a6dd38404a7debe93d4f0a209ecc
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:09:21 -0300

    criar classe LivroService

--- src/main/java/com/zup/Amazup/Livro/Service/LivroService.java
+++ src/main/java/com/zup/Amazup/Livro/Service/LivroService.java
@@ -1,2 +1,12 @@
-package com.zup.Amazup.Livro.Service;public class LivroService {
+package com.zup.Amazup.Livro.Service;
+
+import com.zup.Amazup.Livro.Repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivroService {
+    @Autowired
+    LivroRepository livroRepository;
+
 }
