aa3df77a0553409db6f438c378ed38f9755006ed
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:05:32 -0300

    criar classe AutorRepository

--- src/main/java/com/zup/Amazup/Autor/Repository/AutorRepository.java
+++ src/main/java/com/zup/Amazup/Autor/Repository/AutorRepository.java
@@ -1,2 +1,8 @@
-package com.zup.Amazup.Autor.Repository;public interface AutorRepository {
+package com.zup.Amazup.Autor.Repository;
+
+import com.zup.Amazup.Autor.Autor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AutorRepository extends CrudRepository<Autor, Integer> {
+
 }
