77299990dab83eeaa3c22b000b08a99a0fc1f6b2
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:08:57 -0300

    criar classe LivroController

--- src/main/java/com/zup/Amazup/Livro/Controller/LivroController.java
+++ src/main/java/com/zup/Amazup/Livro/Controller/LivroController.java
@@ -1,2 +1,10 @@
-package com.zup.Amazup.Livro.Controller;public class LivroController {
+package com.zup.Amazup.Livro.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
 }
