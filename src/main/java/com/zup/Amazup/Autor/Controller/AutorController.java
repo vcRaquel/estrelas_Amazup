89e440d9aeee8b2d22f12e1a12c6e47fcd5a0acb
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:05:02 -0300

    criar classe AutorController

--- src/main/java/com/zup/Amazup/Autor/Controller/AutorController.java
+++ src/main/java/com/zup/Amazup/Autor/Controller/AutorController.java
@@ -1,2 +1,10 @@
-package com.zup.Amazup.Autor.Controller;public class AutorController {
+package com.zup.Amazup.Autor.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+public class AutorController {
+
 }
