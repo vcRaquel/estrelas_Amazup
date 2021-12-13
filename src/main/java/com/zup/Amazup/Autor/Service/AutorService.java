5e11fe561eef751ef63154904f3132095b65ba36
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:06:06 -0300

    criar classe AutorService

--- src/main/java/com/zup/Amazup/Autor/Service/AutorService.java
+++ src/main/java/com/zup/Amazup/Autor/Service/AutorService.java
@@ -1,2 +1,12 @@
-package com.zup.Amazup.Autor.Service;public class AutorService {
+package com.zup.Amazup.Autor.Service;
+
+import com.zup.Amazup.Autor.Repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorService {
+    @Autowired
+    private AutorRepository autorRepository;
+
 }
