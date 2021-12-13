17fb22018f08b960ac9dff95d247570bb706ecda
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 14:36:36 -0300

    primeiro commit

---
+++ src/test/java/com/zup/Amazup/AmazupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.Amazup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
