a452d56231670aad17021271dad8571b3d183a66
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:09:49 -0300

    criar classe LivroEntradaDTO

--- src/main/java/com/zup/Amazup/Livro/dto/LivroEntradaDTO.java
+++ src/main/java/com/zup/Amazup/Livro/dto/LivroEntradaDTO.java
@@ -1,2 +1,12 @@
-package com.zup.Amazup.Livro.dto;public class LivroEntradaDTO {
+package com.zup.Amazup.Livro.dto;
+
+import com.zup.Amazup.Autor.Autor;
+import com.zup.Amazup.Livro.enuns.Genero;
+
+public class LivroEntradaDTO {
+
+    private String nome;
+    private Autor autor;
+    private double preco;
+    private Genero genero;
 }
