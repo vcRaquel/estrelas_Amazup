9750e43eca0c321dc7480a985979fde6716f24b2
Raquel Leão <vc.raquel@gmail.com>
2021-12-13 15:07:58 -0300

    criar classe Livro

--- src/main/java/com/zup/Amazup/Livro/Livro.java
+++ src/main/java/com/zup/Amazup/Livro/Livro.java
@@ -1,2 +1,15 @@
-package com.zup.Amazup.Livro;public class Livro {
+package com.zup.Amazup.Livro;
+
+import com.zup.Amazup.Autor.Autor;
+import com.zup.Amazup.Livro.enuns.Genero;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Livro {
+    private String nome;
+    private Autor autor;
+    private double preco;
+    private Genero genero;
+
 }
