e2a7e67ff724184b8a9b4c3ee95ab717ddb4f00f
Raquel Leão <vc.raquel@gmail.com>
2021-12-30 17:00:31 -0300

    criar ControllerAdvice

---
+++ src/main/java/com/zup/Amazup/Config/ControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.zup.Amazup.Config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public void manipularMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+
+    }
+
+}
