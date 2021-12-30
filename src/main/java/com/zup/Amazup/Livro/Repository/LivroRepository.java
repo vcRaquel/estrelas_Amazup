8aeeb69b364e184efbd8499dbe60c499482c21ee
Raquel Leão <vc.raquel@gmail.com>
2021-12-30 17:34:37 -0300

    criar metodo na LivroRepository para verificar se um livro existe pelo nome e pelo id do autor

--- src/main/java/com/zup/Amazup/Livro/Repository/LivroRepository.java
+++ src/main/java/com/zup/Amazup/Livro/Repository/LivroRepository.java
@@ -4,4 +4,6 @@ import com.zup.Amazup.Livro.Livro;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LivroRepository extends CrudRepository<Livro, Integer> {
+
+    boolean existsByNomeAndAutorId(String nome, int id);
 }
