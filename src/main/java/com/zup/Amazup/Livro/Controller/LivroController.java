9006d5079376eb54508ba577d06a38394fe8c520
Raquel Leão <vc.raquel@gmail.com>
2021-12-30 18:16:50 -0300

    criar URI como resposta do metodo cadastrarLivro com o URIConstrutor

--- src/main/java/com/zup/Amazup/Livro/Controller/LivroController.java
+++ src/main/java/com/zup/Amazup/Livro/Controller/LivroController.java
@@ -29,14 +29,9 @@ public class LivroController {
     public HashMap<String, URI> cadastrarLivro(@RequestBody @Valid Livro livro){
         Livro livroObjeto = livroService.salvarLivro(livro);
 
-        //atribuir a uma variável do tipo URI a construção de uma URI feita com o UriComponentsBuilder
-        //que irá criar uma nova instância de URI (newInstance()), definir seu protocolo (scheme("http")),
-        //seu hospedeiro (host("localhost")), sua porta (port(8080)), seu caminho (path("/livros/{id}"))
-        URI enderecoVitrini = UriComponentsBuilder.newInstance()
-                .scheme("http").host("localhost").port(8080)
-                //para atribuir o valor do id do objeto à variable do path,
-                // usamos o buildAndExpand passando como parâmetro o que será acrescentado à URI construída
-                .path("/livros/{id}").buildAndExpand(livro.getId()).toUri();
+        //atribuir a uma variável do tipo URI a construção de uma URI feita com o construtor
+        //(a classe URIConstrutor criada)
+        URI enderecoVitrini = construtor.criarURI(";livros", "" + livro.getId());
 
         HashMap<String, URI> resposta = new HashMap<>();
         resposta.put("vitrine", enderecoVitrini);
