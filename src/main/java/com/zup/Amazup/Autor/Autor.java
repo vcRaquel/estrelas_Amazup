834ad3f8b0dac5cd60a751ae580d45041a0d097d
Raquel Leão <vc.raquel@gmail.com>
2021-12-30 16:55:21 -0300

    nomear tabela de Autores

--- src/main/java/com/zup/Amazup/Autor/Autor.java
+++ src/main/java/com/zup/Amazup/Autor/Autor.java
@@ -1,11 +1,9 @@
 package com.zup.Amazup.Autor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "autores")
 public class Autor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
