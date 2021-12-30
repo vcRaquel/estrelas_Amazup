36b6efd25690fce1b325bad4448c7ef1b7ba2883
Raquel Leão <vc.raquel@gmail.com>
2021-12-29 21:12:35 -0300

    criar AutorDTO

--- src/main/java/com/zup/Amazup/Autor/dto/AutorDTO.java
+++ src/main/java/com/zup/Amazup/Autor/dto/AutorDTO.java
@@ -1,7 +1,25 @@
 package com.zup.Amazup.Autor.dto;
 
+import java.net.URI;
+
 public class AutorDTO {
 
-    private int id;
+    public URI uri;
+    public String nome;
 
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
