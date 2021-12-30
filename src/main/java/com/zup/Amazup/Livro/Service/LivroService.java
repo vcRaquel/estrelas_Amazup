8aae9b157ae2e09a90caec47eaed8fbcce125b7e
Raquel Leão <vc.raquel@gmail.com>
2021-12-30 17:35:20 -0300

    salvar livro Service

--- src/main/java/com/zup/Amazup/Livro/Service/LivroService.java
+++ src/main/java/com/zup/Amazup/Livro/Service/LivroService.java
@@ -1,12 +1,23 @@
 package com.zup.Amazup.Livro.Service;
 
+import com.zup.Amazup.Livro.Livro;
 import com.zup.Amazup.Livro.Repository.LivroRepository;
+import com.zup.Amazup.Livro.customExceptions.LivroJaCadastradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LivroService {
     @Autowired
-    LivroRepository livroRepository;
+    private LivroRepository livroRepository;
+
+    public Livro salvarLivro(Livro livro){
+        if (livroRepository.existsByNomeAndAutorId(livro.getNome(), livro.getAutor().getId())) {
+            throw new LivroJaCadastradoException();
+        }
+        Livro livroObjeto = livroRepository.save(livro);
+
+        return livroObjeto;
+    }
 
 }
