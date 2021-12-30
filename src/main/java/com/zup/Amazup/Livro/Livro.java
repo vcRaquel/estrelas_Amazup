af039461d3ffb77b87d46cef5e9afebf59fd91b2
Raquel Leão <vc.raquel@gmail.com>
2021-12-29 21:10:12 -0300

    criar entidade de Livro

--- src/main/java/com/zup/Amazup/Livro/Livro.java
+++ src/main/java/com/zup/Amazup/Livro/Livro.java
@@ -3,11 +3,15 @@ package com.zup.Amazup.Livro;
 import com.zup.Amazup.Autor.Autor;
 import com.zup.Amazup.Livro.enuns.Genero;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String nome;
+    @ManyToOne
     private Autor autor;
     private double preco;
     private Genero genero;
